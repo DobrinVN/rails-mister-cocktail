824b0b9e3e12d568c4582ca4e0c2d67702c6a611
DobrinVN <dob.nedyalkov@gmail.com>
2020-08-14 12:46:32 +0100

    finished the cocktail app with all functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # GET "cocktails" to: "cocktails#index"
+  # GET "cocktails/new" to: "cocktails#new", as: :new_cocktail
+  # POST "cocktails" to: "cocktails#create"
+  # GET "cocktails/:id" to: "cocktails#show", as: :cocktail
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
